956cf41729db2d72da5d6e46920b302241218649
Patrick Bosworth <bosworth.patrick@gmail.com>
2018-03-27 08:01:01 +0100

    new get route added for retrieving individual items

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const {ObjectID} = require('mongodb')
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -27,6 +29,25 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+//GET /todos/123123
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then( (todo) => {
+      if (!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+    console.log('sent todo back');
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('started on port 3000');
