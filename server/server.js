de9c6cdbd2a9f7bbab52080834166f3a053b8264
Patrick Bosworth <bosworth.patrick@gmail.com>
2018-03-31 09:40:27 +0100

    added delete test todos

--- server/server.js
+++ server/server.js
@@ -49,26 +49,20 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req,res) => {
-  //get the id
   var id = req.params.id;
-  //validate the id -> not valid return 404
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   };
+
   Todo.findByIdAndRemove(id).then( (todo) => {
     if (!todo) {
-      res.status(404).send();
+      return res.status(404).send();
     }
-    res.status(200).send({todo});
+    
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
-  //remove todo by id
-    //success
-      //if no doc send 404
-      //if doc send back with 200
-    //error
-      //400 with empty body
 })
 
 
